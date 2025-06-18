c2e0931e429067b7caa623f0a48990cd5836dc62
Marco Bellezza <jasone.mb@outlook.com>
2025-06-18 23:11:54 +0200

    support build

--- modules/new.ts
+++ modules/new.ts
@@ -1,4 +1,4 @@
-import { $try } from '.';
+import { $try } from './index';
 
 type Constructor<Args extends unknown[], Instance> = new (...args: Args) => Instance;
 export const $new = <Args extends unknown[], Instance>(c: Constructor<Args, Instance>, ...args: Args) => $try.sync(() => new c(...args));
