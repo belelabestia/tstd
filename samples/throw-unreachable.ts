67d5d2020c58e2c121bb583c50804e762833f064
Marco Bellezza <jasone.mb@outlook.com>
2025-06-18 22:34:02 +0200

    update

--- samples/throw-unreachable.ts
+++ samples/throw-unreachable.ts
@@ -1,29 +1,14 @@
-import { $try } from '../modules';
-import { branch } from '../modules/branch';
 import { unreachable } from '../modules/unreachable';
 
 export const throwUnreachable = () => {
-  console.log('here i have an operation that might fork');
-  const toughDecision = () => {
-    const ran = Math.random();
-
-    if (ran < 0.01) return branch('unlikely low', null);
-    if (ran > 0.99) return branch('unlikely high', null);
-    return branch('likely', 1);
-  };
-
-  console.log('but i only care about the likely branch');
-  const res = toughDecision();
-
-  console.log('then i can unsafely assert that the unlikely branch is unreachable');
-  $try.sync(() => { if (res.branch !== 'likely') throw unreachable(); });
-
   console.log('unreachable is the only error you need to throw');
   const logicAssertion = () => {
     const ran = Math.random();
 
-    console.log('this code is actually unreachable');
-    if (ran < 0) throw unreachable();
+    if (ran < 0) {
+      console.log('this code is actually unreachable');
+      throw unreachable();
+    }
   };
 
   console.log('you should not be concerned to make an assertion');
