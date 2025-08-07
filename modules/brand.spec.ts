dd515b0dcc660bdbfd9dc80f877f79dee8aaa1d3
Marco Bellezza <jasone.mb@outlook.com>
2025-08-07 16:56:12 +0200

    add brand tests

--- modules/brand.spec.ts
+++ modules/brand.spec.ts
@@ -1,27 +1,35 @@
+import { test } from 'node:test';
+import * as assert from 'node:assert/strict';
 import { Brand } from './brand';
 
-// type branding is a very clever technique, but it's usually overengineered;
-// all we really need is the brand, not the branded type.
-type Int = number & Brand<'Int'>;
-declare const int: Int;
+test("brand stuff", () => {
+  // type branding is a very clever technique, but it's usually overengineered;
+  // all we really need is the brand, not the branded type
 
-// this decouples the trait or requirement you're trying to mark from the runtime type
-// and makes it easier to involve brands in type algebra:
-type Truhty = Brand<'Truthy'>;
-type Trimmed = string & Brand<'Trimmed'>;
-type Valid = (number | Trimmed) & Truhty;
+  // this decouples the trait or requirement you're trying to mark from the runtime type
+  // and makes it easier to involve brands in type algebra:
+  type Truthy = Brand<'Truthy'>;
+  type Trimmed = string & Brand<'Trimmed'>;
+  type Valid = (number | Trimmed) & Truthy;
 
-// after you've defined your branded types you can create type guards for them
-const number = (x: unknown) => typeof x === 'number';
-const truthy = (x: unknown): x is Truhty => Boolean(x);
-const trimmed = (x: unknown): x is Trimmed => typeof x === 'string' && x.trim() === x;
+  // usually, branded types are used together with type assertions (using "as")
+  // but in tstd we prefer using brands together with type guards
+  const number = (x: unknown) => typeof x === 'number';
+  const truthy = (x: unknown): x is Truthy => Boolean(x);
+  const trimmed = (x: unknown): x is Trimmed => typeof x === 'string' && x.trim() === x;
 
-// this way, boolean algebra and type algebra match perfectly:
-const valid = (x: unknown): x is Valid => (number(x) || trimmed(x)) && truthy(x);
+  // this way, boolean algebra and type algebra match perfectly:
+  const valid = (x: unknown): x is Valid => (number(x) || trimmed(x)) && truthy(x);
 
-// now you can be very specific about function parameter types:
-const veryStrictFunction = (x: Valid) => {};
+  // now you can be very specific about function parameter types:
+  const veryStrictFunction = (_: Valid) => { };
 
-// and you can only pass runtime-validated values without ever doing a type assertion:
-declare const x: unknown;
-if (valid(x)) veryStrictFunction(x);
+  // and you can only pass runtime-validated values without ever doing a type assertion:
+  const x1 = 2;
+  if (valid(x1)) veryStrictFunction(x1);
+  else assert.fail();
+
+  if (valid(0)) assert.fail();
+  if (valid("")) assert.fail();
+  if (valid(new Date())) assert.fail();
+});
