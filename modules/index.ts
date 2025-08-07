d21e2b17dd7d7439b2a955ba9b13c9d12a63b098
Marco Bellezza <jasone.mb@outlook.com>
2025-08-07 17:02:02 +0200

    update index

--- modules/index.ts
+++ modules/index.ts
@@ -1,11 +1,10 @@
-// modules with explicit naming, like `branch` or `Prettify<T>`
+// modules with global naming, like `branch` or `Prettify<T>`
 export * from './branch';
 export * from './brand';
 export * from './make';
 export * from './prettify';
-export * from './fail';
 
-// modules with implicit naming, like `defer.sync/async` or `is.array/record/etc`
+// modules with scoped naming, like `defer.sync/async` or `is.array/record/etc`
 export * as defer from './defer';
 export * as is from './is';
 export * as task from './task';
