bd6d4bfcd0b9248316d8251291bc5bb8a17d4cb8
Marco Bellezza <jasone.mb@outlook.com>
2025-08-18 23:45:40 +0200

    simplify is

--- modules/is.spec.ts
+++ modules/is.spec.ts
@@ -1,20 +1,89 @@
+import { test } from 'node:test';
+import * as assert from 'node:assert/strict';
 import * as is from './is';
 
-const a = () => {
-  const ops = () => { throw ' ' };
+/*
+  schema validation doesn't have to be a complicated task;
+  thanks to ts type narrowing we can use type guards as lightweight schema validators
+*/
 
-  try {
-    ops();
-  }
-  catch (err) {
-    if (is.model(err, { message: is.$string })) {
-      console.log(err.message);
+test('safely navigate the unknown', () => {
+  // here's a bunch of unknown variables
+  const a: unknown = 1, b: unknown = '_';
+
+  // let's narrow them down
+  if (!is.number(a)) assert.fail();
+  if (!is.string(b)) assert.fail();
+
+  // now a is a number, and b is a string
+  assert.equal(a, 1);
+  assert.equal(b, '_');
+});
+
+test('validate model schemas', () => {
+  // here's a more complex unkown variable
+  const a: unknown = {
+    a: 1,
+    b: {
+      a: '_',
+      b: false
     }
+  };
 
-    const string = Schema.string();
+  // let's validate it
+  const schema = {
+    a: is.number,
+    b: (x: unknown) => is.model(x, {
+      a: is.string,
+      b: is.boolean
+    })
+  };
 
-    if (Schema.validate(string, err)) {
-      console.log(err);
-    }
-  }
-};
+  if (!is.model(a, schema)) assert.fail();
+
+  // look at the type of a
+  assert.equal(a.a, 1);
+  assert.equal(a.b.b, false);
+
+  // you can also validate an array of models
+  assert.ok(is.models([a], schema));
+});
+
+test('validate combined types', () => {
+  // in typescript you can algebraically combine types
+  type Union = string | number;
+
+  // there isn't a built-in `union` in tstd
+  // however, combining type guards is straightforward
+  const isUnion = (x: unknown): x is Union => is.string(x) || is.number(x);
+  assert.ok(isUnion('a'));
+  assert.ok(isUnion(1));
+  assert.ok(!isUnion(false));
+
+  // same goes for literals
+  // you're free to implement as you wish
+  // typescript will infer this
+  const isBBool = (x: unknown) => x === 'a' || x === 'b' || x === 'c';
+  assert.ok(isBBool('a'));
+  assert.ok(!isBBool('d'));
+
+  // this won't be inferred
+  const isBArrIncl = (x: unknown): x is 'a' | 'b' | 'c' => is.string(x) && ['a', 'b', 'c'].includes(x);
+  assert.ok(isBArrIncl('a'));
+  assert.ok(!isBArrIncl('d'));
+
+  // you can combine as you wish
+  // as long as you use boolean logic, typescript will infer
+  const isCombined = (x: unknown) =>
+    x === null || (
+      is.model(x, { message: is.string }) &&
+      is.model(x, {
+        payload: x => is.models(x, { data: is.number })
+      })
+    );
+
+  assert.ok(isCombined(null));
+  assert.ok(isCombined({ message: 'hello', payload: [{ data: 1 }, { data: 2 }] }));
+  assert.ok(!isCombined({ message: 1 }));
+  assert.ok(!isCombined({ message: 'hello', payload: [{ data: [1, 2] }] }));
+});
--- modules/is.ts
+++ modules/is.ts
@@ -1,33 +1,31 @@
-// This module is all about types.
-// We have a few general definitions followed by a collection of branded types and a bunch of built-in type guards.
-// Extend this collection by creating your own branded types and respective guards in your project.
-
-import { Brand } from './brand';
-
-/** A type-narrowing function. */
+/** a type-narrowing function */
 export type TypeGuard<T> = (x: unknown) => x is T;
 
-/** An object describing a structure. */
+/** an object describing a structure */
 export type Schema = Record<string, TypeGuard<unknown>>;
 
-/** The actual type coming out of a schema validation. */
+/** the actual validated type */
 export type Model<T extends Schema> = { [K in keyof T]: T[K] extends TypeGuard<infer U> ? U : never };
 
-export type Truthy = Brand<'Truthy'>;
-export type DateString = string & Brand<'DateString'>;
+/* mind that some of this functions override the expected js behavior */
 
-export type JsonValue =
-  | string
-  | number
-  | boolean
-  | null
-  | undefined
-  | JsonObject
-  | JsonArray;
+export const boolean = (x: unknown): x is boolean =>
+  typeof x === 'boolean';
 
-export type JsonObject = { [key: string]: JsonValue };
-export type JsonArray = JsonValue[];
-export type Json = JsonObject | JsonArray;
+export const number = (x: unknown): x is number =>
+  typeof x === 'number' &&
+  Number.isFinite(x);
+
+export const string = (x: unknown): x is string =>
+  typeof x === 'string';
+
+export const present = (x: unknown): x is {} =>
+  x !== undefined &&
+  x !== null;
+
+export const absent = (x: unknown): x is undefined | null =>
+  x === undefined ||
+  x === null;
 
 export const record = (x: unknown): x is Record<string, unknown> =>
   typeof x === 'object' &&
@@ -39,68 +37,13 @@ export const array = (x: unknown): x is unknown[] =>
 export const model = <T extends Schema>(x: unknown, schema: T): x is { [K in keyof Model<T>]: Model<T>[K] } => {
   if (!record(x)) return false;
 
-  for (const key in schema)
-    if (!schema[key](x[key])) return false;
-
+  for (const key in schema) if (!schema[key](x[key])) return false;
   return true;
 };
 
-export const or = <T extends TypeGuard<unknown>[]>(...guards: T) => (x: unknown): x is T[number] extends TypeGuard<infer U> ? U : never => {
-  for (let i = 0; i < guards.length; i++)
-    if (guards[i](x)) return true;
+export const models = <T extends Schema>(x: unknown, schema: T): x is { [K in keyof Model<T>]: Model<T>[K] }[] => {
+  if (!array(x)) return false;
 
-  return false;
-};
-
-export const and = <T extends TypeGuard<unknown>[]>(...guards: T) => (x: unknown): x is T[number] extends TypeGuard<infer U> ? U : never => {
-  for (let i = 0; i < guards.length; i++)
-    if (!guards[i](x)) return false;
-
+  for (let i = 0; i < x.length; i++) if (!model(x[i], schema)) return false;
   return true;
-}
-
-export const jsonValue = (x: unknown): x is JsonValue =>
-  typeof x === 'string' ||
-  typeof x === 'number' ||
-  typeof x === 'boolean' ||
-  x === null ||
-  x === undefined ||
-  jsonObject(x) ||
-  jsonArray(x);
-
-export const jsonObject = (x: unknown): x is JsonObject =>
-  record(x) &&
-  Object.entries(x).every(([k, v]) =>
-    typeof k === 'string' &&
-    jsonValue(v)
-  );
-
-export const jsonArray = (x: unknown): x is JsonArray =>
-  Array.isArray(x) &&
-  x.every(jsonValue);
-
-export const json = (x: unknown): x is Json =>
-  jsonObject(x) ||
-  jsonArray(x);
-
-export const string = (x: unknown): x is string =>
-  typeof x === 'string';
-
-export const truthyNumber = (x: unknown): x is number & Truthy =>
-  typeof x === 'number' &&
-  x !== 0 &&
-  !Number.isNaN(x);
-
-export const dateString = (x: unknown): x is DateString => {
-  if (!string(x)) return false;
-
-  const regex = /^\d{4}-\d{2}-\d{2}$/;
-  if (!regex.test(x)) return false;
-
-  const [year, month, day] = x.split('-').map(Number);
-  const date = new Date(year, month - 1, day);
-
-  return date.getFullYear() === year &&
-    date.getMonth() === month - 1 &&
-    date.getDate() === day;
 };
