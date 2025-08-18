d569904f29498595b150b9427564d7666ceddba7
Marco Bellezza <jasone.mb@outlook.com>
2025-08-18 23:43:38 +0200

    update task

--- modules/task.ts
+++ modules/task.ts
@@ -1,12 +1,12 @@
 import { result } from './result';
 
-/** Wraps a sync try-catch block. */
+/** turns a sync try-catch block into a result */
 export const sync = <T extends unknown>(f: () => T) => {
   try { return result.success(f()) }
   catch (error) { return result.error(error as unknown) }
 };
 
-/** Wraps an async try-catch block. */
+/** turns an async try-catch block into a promise of result */
 export const async = async <T extends Promise<unknown>>(f: () => Promise<T>) => {
   try { return result.success(await f()) }
   catch (error) { return result.error(error as unknown) }
